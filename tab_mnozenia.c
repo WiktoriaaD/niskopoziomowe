604d8de2d02a712c287a92afbd7267686a45bc25
WiktoriaaD <dulskaw@gmail.com>
2025-02-26 18:11:03 +0100

    Update tab_mnozenia.c

--- tab_mnozenia.c
+++ tab_mnozenia.c
@@ -1,25 +1,46 @@
 #include <stdio.h>
 int main(int argc, char *argv[]){
-    printf("Podaj wartość: ");
+    printf("Podaj wartość/wartosci: ");
     int wartosc;
     scanf("%d", &wartosc);
     int padding = 1;
     int liczba=wartosc*wartosc;
     int j;
     int i;
+    if (argc>1) 
+    do{
+        liczba/=16;
+        padding++;
+    }while (liczba!=0);
+    else 
     do{
         liczba/=10;
         padding++;
     }while (liczba!=0);
-    for (i = 1; i <= wartosc; i++)
+
+    if(argc>1)
     {
-        for (j = 1; j <= wartosc; j++)
+        for (i = 1; i <= wartosc; i++)
         {
-            printf("%*X",padding, i * j);
-        }
-        printf("\n");
+            for (j = 1; j <= wartosc; j++)
+            {
+                printf("%*X",padding, i * j);
+            }
+            printf("\n");
         
+        }
+    }
+    else
+    {
+        for (i = 1; i <= wartosc; i++)
+        {
+            for (j = 1; j <= wartosc; j++)
+            {
+                printf("%*d",padding, i * j);
+            }
+            printf("\n");
+        
+        }
     }
     return 0;
-    
 }
